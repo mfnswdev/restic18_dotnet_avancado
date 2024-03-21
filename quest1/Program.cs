1285ceac304a3a0acff084a5f9c4c730a799a1b3
Giuseppe Mota <giuseppemota@gmail.com>
2024-03-20 22:07:47 -0300

    Update project references in Dot_avançado.sln

---
+++ quest1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        List<double> Lista = new List<double> { 3, 7, 4, 2, 6 };
+        List<double> novaLista = new List<double>();
+
+        novaLista = Lista.ConvertAll(x => x / 2);
+
+        novaLista.ForEach(x => Console.WriteLine(x));
+
+    }
+
+}
