96f5c8298f0745b6864516e4ffe044d24d04eddf
mfnswdev <engenharia.mfn@gmail.com>
2024-03-20 21:45:38 -0300

    add Lista 3 de exercicios

---
+++ dotnet_avancado_exercicio3/exercicio_Linq/LINQ/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    public class ItemMercado
+    {
+        public string Nome { get; set; }
+        public Tipo Tipo { get; set; }
+        public double Preco { get; set; }
+    }
+
+    public enum Tipo { Comida, Bebida, Higiene, Limpeza };
+
+    static void Main(string[] args)
+    {
+        List<ItemMercado> ListaMercado = new List<ItemMercado> {
+            new ItemMercado { Nome = "Arroz", Tipo = Tipo.Comida, Preco = 3.90 },
+            new ItemMercado { Nome = "Azeite", Tipo = Tipo.Comida, Preco = 2.50 },
+            new ItemMercado { Nome = "Macarrao", Tipo = Tipo.Comida, Preco = 3.90 },
+            new ItemMercado { Nome = "Cerveja", Tipo = Tipo.Bebida, Preco = 22.90 },
+            new ItemMercado { Nome = "Refrigerante", Tipo = Tipo.Bebida, Preco = 5.50 },
+            new ItemMercado { Nome = "Shampoo", Tipo = Tipo.Higiene, Preco = 7.00 },
+            new ItemMercado { Nome = "Sabonete", Tipo = Tipo.Higiene, Preco = 2.40 },
+            new ItemMercado { Nome = "Cotonete", Tipo = Tipo.Higiene, Preco = 5.70 },
+            new ItemMercado { Nome = "Sabão em Pó", Tipo = Tipo.Limpeza, Preco = 8.20 },
+            new ItemMercado { Nome = "Detergente", Tipo = Tipo.Limpeza, Preco = 2.60 },
+            new ItemMercado { Nome = "Amaciante", Tipo = Tipo.Limpeza, Preco = 6.40 },
+        };
+
+        ListaMercado.Where(x => x.Tipo == Tipo.Higiene).OrderByDescending(x => x.Preco).ToList().ForEach(x => Console.WriteLine($"Nome: {x.Nome} | Tipo: {x.Tipo} | Preço: {x.Preco}"));
+        ListaMercado.Where(valor => valor.Preco >= 5.00).OrderBy(x => x.Preco).ToList().ForEach(x => Console.WriteLine($"Nome: {x.Nome} | Tipo: {x.Tipo} | Preço: {x.Preco}"));
+        ListaMercado.Where(tipo => tipo.Tipo == Tipo.Bebida || tipo.Tipo == Tipo.Comida).OrderBy(x => x.Nome).ToList().ForEach(x => Console.WriteLine($"Nome: {x.Nome} | Tipo: {x.Tipo} | Preço: {x.Preco}"));
+        ListaMercado.GroupBy(item => item.Tipo).Select(group => new { Tipo = group.Key, Quantidade = group.Count() }).ToList().ForEach(x => Console.WriteLine($"Tipo: {x.Tipo} | Quantidade: {x.Quantidade}"));
+        ListaMercado.GroupBy(item => item.Tipo).Select(group => new
+        {
+            Tipo = group.Key,
+            PrecoMaximo = group.Max(item => item.Preco),
+            PrecoMinimo = group.Min(item => item.Preco),
+            PrecoMedio = group.Average(item => item.Preco)
+        }).ToList().ForEach(x => Console.WriteLine($"Tipo: {x.Tipo} | Preco Maximo: {x.PrecoMaximo} | Preco Minimo: {x.PrecoMinimo} | Preco Medio: {x.PrecoMedio}"));
+    }
+}
---
+++ dotnet_avancado_exercicio3/exercicio_Threads/Threads/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread thread1 = new Thread(new Worker().Work);
+        Thread thread2 = new Thread(new Worker().Work);
+
+        Console.WriteLine("Iniciando threads...");
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("Trabalho concluído. Programa encerrado.");
+    }
+
+    class Worker
+    {
+        public void Work()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Trabalho em progresso ({i}/5)");
+                Thread.Sleep(1000); // Simula algum tipo de trabalho
+            }
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Trabalho concluído.");
+        }
+    }
+}
