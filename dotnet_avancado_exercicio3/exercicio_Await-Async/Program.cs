6219818df06ba84ed02c66921cdf5b0aa14bc462
mfnswdev <engenharia.mfn@gmail.com>
2024-03-22 09:44:08 -0300

    add Exercicio Await-Async

---
+++ dotnet_avancado_exercicio3/exercicio_Await-Async/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Task task1 = DoWorkAsync("Tarefa 1");
+        Task task2 = DoWorkAsync("Tarefa 2");
+
+        await Task.WhenAll(task1, task2);
+
+
+        Console.WriteLine("Ambas as tarefas foram concluídas.");
+    }
+
+    static async Task DoWorkAsync(string taskName)
+    {
+        for (int i = 1; i <= 5; i++) 
+        {
+            Console.WriteLine($"Executando {taskName}, Iteração {i}");
+            await Task.Delay(1000); 
+        }
+    }
+}
